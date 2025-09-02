cf99d9a01a3a39c2abb0d0236f7a6f72b99b0fd2
Pat-Zhu <xihanz0108@outlook.com>
2025-09-02 19:37:26 +0000

    3rd com with modification on test_hello

--- test_hello.py
+++ test_hello.py
@@ -1,4 +1,4 @@
-from hello import say_hello, add
+from hello import say_hello, add, multiply
 
 def test_say_hello():
     assert (
@@ -6,5 +6,9 @@ def test_say_hello():
         == "Hello, Annie, welcome to Data Engineering Systems (IDS 706)!"
     )
 
+
 def test_add():
     assert add(2, 3) == 5
+
+def test_multiply():
+    assert multiply(5,10)==50
